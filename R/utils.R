ce4601d895087a6364d202ac50e3edb43dbb9973
Jerônimo <jeronimo_acker@hotmail.com>
2021-04-23 19:40:01 -0300

    add funções auxiliares

--- R/utils.R
+++ R/utils.R
@@ -11,7 +11,166 @@ nvalid <- function(x) {
 }
 
 
+#' Calculate the data mean
+#'
+#' @param x a numeric vector
+#'
+#' @return mean value of x
+#'
+
 mean_wise <- function(x){
   if(all(is.na(x))) return(NA)
   mean(x, na.rm = TRUE)
 }
+
+#' Select a hydroelectric station
+#'
+#' @param df a tibble or data frame
+#' @param station station code
+#' @return tibble or data frame of the selected station
+#'
+
+sel_station <- function(df,station){
+  # Função para selecionar um posto específico via código
+  df %>% 
+    dplyr::filter(code_stn == station) 
+}
+
+
+apply_cmonth <- function(df){
+  # Função para filtrar os meses completos dos dados 
+  df <- df %>% 
+    dplyr::group_by(date = floor_date(date, "month"), code_stn) %>% 
+    dplyr::summarise(
+      qnat_obs = mean_wise(qnat), 
+      valid = nvalid(qnat), 
+      N = n(),
+      .groups = "drop"
+    ) %>%
+    dplyr::filter(valid >= 28) %>% 
+    select(date,code_stn,qnat_obs)
+  return(df)
+}
+
+get_cyears <- function(df){
+  # Função para pegar os anos completos dos dados
+  cyrs <- df %>% 
+    group_by(ano = lubridate::year(date)) %>% 
+    tally() %>% 
+    filter(n == 12) %>% 
+    pull(ano)
+  return(cyrs)
+}
+
+apply_cyears  <- function(df){
+  # Função para filtrar os dados com anos completos
+  # Usar depois de agrupar os dados caso selecionado mais de um posto
+  cyrs <- get_cyears(df)
+  df_cyrs <- df %>% 
+    filter(lubridate::year(date) %in% cyrs)
+  return(df_cyrs)
+}
+
+
+get_traindt <- function(df){
+  # Função para pegar os dados de treinamento
+  # Padrão: 2 últimos anos de observações são removidos
+  cyrs <- get_cyears(df)
+  leave_out <- tail(cyrs,n = 2) # retira 2 últ. anos por padrão
+  data <- df %>% 
+    filter(!lubridate::year(date) %in% leave_out)
+  return(data)
+}
+
+
+get_testdt <- function(df){
+  # Função para pegar os dados de teste
+  # Padrão: 2 últimos anos de observações são usados como teste
+  nahead = 24
+  inds <- (nrow(df) - nahead + 1):(nrow(df))
+  df <- df[inds,] %>% 
+    select(date,qnat_obs)
+  #df[inds,"qnat_obs"] # df[inds,2]
+}
+
+
+psf_reprod <- function(df){
+  # Função para aplicar o psf, retorna as predições do modelo
+  set.seed(1) # p/ reprodutibilidade
+  model <- psf(df[,"qnat_obs"],cycle = 12) #df[,2]
+  preds <- predict(model,n.ahead = 24)
+  return(preds)
+}
+
+psf4ensemble <- function(df){
+  # Função para aplicar o psf para ensemble
+  model <- psf(df[,"qnat_obs"],cycle = 12) #df[,2]
+  preds <- predict(model,n.ahead = 24)
+  return(preds)
+}
+
+
+
+ensemble_preds <- function(df){
+  # Retorna uma lista com as predições do psf com difs seeds
+  list_preds <- list()
+  # list_model <- list()
+  for(i in 1:5){
+    set.seed(i)
+    list_preds[[i]] <- psf4ensemble(df)
+  }
+  return(list_preds)
+}
+
+
+
+ensemble_models <- function(df){
+  # Retorna uma lista com os modelos do psf com difs seeds
+  list_model <- list()
+  for(i in 1:5){
+    set.seed(i)
+    model <- psf(df[,"qnat_obs"],cycle = 12)
+    list_model[[i]] <- model
+  }
+  return(list_model)
+  
+}
+
+ensemble_mpar <- function(df,params){
+  # Retorna predições feitas com os valores médios de k e w
+  # extraídos usando a função get_mpar
+  set.seed(1)
+  model <- psf(df[,"qnat_obs"],
+               k = params[[1]][[1]],
+               w = params[[1]][[2]],
+               cycle = 12)
+  pred <- predict(model,n.ahead = 24)
+  return(pred)
+}
+
+get_mpar <- function(modelo){
+  # Média dos parâmetros k e w dos modelos
+  list_k <- list()
+  list_w <- list()
+  teste <- for(i in 1:5){
+    list_k[[i]] <- modelo[[i]]$k
+    list_w[[i]] <- modelo[[i]]$w
+  }
+  # Usar apenas números inteiros no modelo
+  k_mean <-  round(Reduce("+",list_k)/length(list_k),0)
+  w_mean <- round(Reduce("+",list_w)/length(list_w),0)
+  list_param <- list()
+  list_param[[1]] <- c(k_mean,w_mean)
+}
+
+
+get_mpred <- function(lista){
+  # Faz a média das predições dos modelos
+  pred_mean <- list(
+    rowMeans(as.data.frame(lista))
+  )
+}
+
+
+
+
