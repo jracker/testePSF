ecd18cc6e05f064e84a39ded8d5a7eff6d2d966a
Jerônimo <jeronimo_acker@hotmail.com>
2021-05-13 21:08:26 -0300

    separate mean and mode function

--- R/utils.R
+++ R/utils.R
@@ -11,17 +11,11 @@ nvalid <- function(x) {
   sum(!is.na(x))
 }
 
-# Calcula a moda 
-moda <- function(x){
-  which.max(tabulate(x))
-}
-
-
 #' Média com tratamento para caso de todos dados faltantes
 #'
-#' @param x a numeric vector
+#' @param x um vetor numérico
 #'
-#' @return mean value of x
+#' @return valor médio de x
 #'
 
 mean_wise <- function(x) {
@@ -31,11 +25,25 @@ mean_wise <- function(x) {
   mean(x, na.rm = TRUE)
 }
 
+
+#' Calcula a moda 
+#' 
+#' @param x um vetor numérico
+#' 
+#' @return um inteiro correspondente a moda do vetor numérico
+#' 
+moda <- function(x){
+  which.max(tabulate(x))
+}
+
+
 #' Seleciona dados de um posto
 #'
-#' @param df a tibble or data frame
-#' @param station station code
-#' @return tibble or data frame of the selected station
+#' @param df um tibble ou data frame
+#' 
+#' @param station código da estação
+#' 
+#' @return tibble ou data frame da estação selecionada
 #'
 sel_station <- function(df, station) {
   # Função para selecionar um posto específico via código
@@ -44,6 +52,13 @@ sel_station <- function(df, station) {
 }
 
 #' Filtra os meses completos dos dados
+#' 
+#'  @param df um tibble ou data frame
+#'  
+#'  @param ndays_thresh  um inteiro
+#'  
+#'  @return um tibble ou data frame com os meses completos
+#'  
 apply_cmonth <- function(df, ndays_thresh = 28) {
   df <- df %>%
     dplyr::group_by(date = floor_date(date, "month"), code_stn) %>%
@@ -58,7 +73,12 @@ apply_cmonth <- function(df, ndays_thresh = 28) {
   return(df)
 }
 
-#' Identifica anos completos, ou seja com 12 meses de observações válidas.
+#' Identifica anos completos, ou seja, com 12 meses de observações válidas
+#'  
+#' @param df um tibble ou data frame
+#' 
+#' @return vetor com os anos que apresentam 12 meses de observações válidas
+#' 
 get_cyears <- function(df) {
   cyrs <- df %>%
     group_by(ano = lubridate::year(date)) %>%
@@ -69,8 +89,14 @@ get_cyears <- function(df) {
 }
 
 #' Filtragem dos dados com anos completos
+#'  
+#'  @param df um tibble ou data frame
+#'  
+#'  @param ndays_thresh  um inteiro
+#'  
+#'  @return df um tibble ou data frame com os meses completos
+#'  
 apply_cyears <- function(df) {
-
   # Usar depois de agrupar os dados caso selecionado mais de um posto
   cyrs <- get_cyears(df)
   df_cyrs <- df %>%
@@ -81,8 +107,11 @@ apply_cyears <- function(df) {
 #' Seleciona dados de treinamento para aplicação do PSF
 #'
 #' @param df data frame com série mensal dos dados de vazão
-#' @param yrs número de anos que serão removidos das observações para teste do PSF.
+#' 
+#' @param yrs número de anos que serão removidos das observações para teste do 
+#' PSF.
 #' Valor pré-definido como 2 anos.
+#' 
 get_traindt <- function(df, yrs = 2) {
   cyrs <- get_cyears(df)
   leave_out <- tail(cyrs, n = yrs)
@@ -101,6 +130,7 @@ get_traindt <- function(df, yrs = 2) {
 #' @export
 #'
 #' @examples
+#' 
 get_testdt <- function(df, n = 24) {
   inds <- (nrow(df) - n + 1):(nrow(df))
   df <- df[inds, ] %>%
@@ -110,14 +140,16 @@ get_testdt <- function(df, n = 24) {
 
 #' Aplica PSF e retorna o modelo ou suas previsões
 #'
-#' @param df 
-#' @param n 
-#' @param predict 
+#' @param df um tibble ou data frame
+#' @param n número de meses à frente. Pré-definido como 24.
+#' @param predict valor lógico pré-definido para retornar as previsões
 #'
-#' @return
+#' @return vetor numérico com as previsões ou objeto da classe psf com 
+#' informações do modelo
 #' @export
 #'
 #' @examples
+#' 
 psf_reprod <- function(df, n = 24, predict = TRUE) {
   
   set.seed(1) # p/ reprodutibilidade
@@ -134,17 +166,17 @@ psf_reprod <- function(df, n = 24, predict = TRUE) {
 #' Aplica o PSF e retorna o modelo ou as previsões
 #' Adaptado para uso no ensemble
 #'
-#' @param df 
-#' @param n 
-#' @param predict 
+#' @param df um tibble ou data frame
+#' @param n número de meses à frente. Pré-definido como 24.
+#' @param predict valor lógico pré-definido para retornar as previsões
 #'
-#' @return
+#' @return vetor numérico com as previsões ou objeto da classe psf com 
+#' informações do modelo 
 #' @export
 #'
 #' @examples
 
 psf4ensemble <- function(df, n = 24, predict = TRUE) {
-  # Função para aplicar o psf para ensemble
   model <- psf(df[, "qnat_obs"], cycle = 12)
   preds <- predict(model, n.ahead = n)
   if (predict) {
@@ -155,6 +187,21 @@ psf4ensemble <- function(df, n = 24, predict = TRUE) {
 }
 
 
+#' Aplica o PSF um número de vezes e retorna os modelos ou as 
+#' previsões
+#' @param df um tibble ou data frame
+#' 
+#' @param niter iterações que definem número de modelos ou de previsões
+#' 
+#' @param predict valor lógico pré-definido para retornar as previsões
+#'
+#' @return lista das previsões ou objetos de classe psf com 
+#' informações do modelos 
+#' 
+#' @export
+#'
+#' @examples
+#' 
 ensemble_psf <- function(df, niter = 5, predict = TRUE) {
   list_preds <- list()
   list_model <- list()
@@ -173,9 +220,21 @@ ensemble_psf <- function(df, niter = 5, predict = TRUE) {
   }
 }
 
+#' Aplica o PSF utilizando os parâmetros k e w selecionados
+#' 
+#' @param df um tibble ou data frame
+#' 
+#' @param params lista de tibbles contendo os valores k e w
+#' 
+#' @param predict valor lógico pré-definido para retornar as previsões
+#'
+#' @return lista das previsões ou objetos de classe psf com 
+#' informações do modelos 
+#' @export
+#'
+#' @examples
+#' 
 ensemble_mpar<- function(df, params, n = 24) {
-  # Retorna predições feitas com os valores médios de k e w
-  # extraídos usando a função get_mpar
   set.seed(1)
   model <- psf(df[, "qnat_obs"],
                k = params[[1]]["k"][[1]],
@@ -186,32 +245,40 @@ ensemble_mpar<- function(df, params, n = 24) {
   return(pred)
 }
 
+#' Seleciona os parâmetros k e w com base na moda
+#' 
+#' @param modelo uma lista de objetos da classe psf
+#' 
+#' @param niter número de modelos ou de previsões
+#' 
+#'
+#' @return lista de tibbles com os valores k e w selecionados
+#' 
+#' @export
+#'
+#' @examples
+#' 
 
-get_mpar <- function(modelo, niter = 5) {
-  
-  # modelo <- pobqmly_esb$models[[1]]; niter = 5
-  
-  # Média dos parâmetros k e w dos modelos
-  # list_k <- list()
-  # list_w <- list()
-  # for (i in 1:niter) {
-  #   list_k[[i]] <- modelo[[i]]$k
-  #   list_w[[i]] <- modelo[[i]]$w
-  # }
-  # Usar apenas números inteiros no modelo
-  #k_mean <- round(Reduce("+", list_k) / length(list_k), 0)
-  #w_mean <- round(Reduce("+", list_w) / length(list_w), 0)
-  
+get_modpar <- function(modelo) {
   tibble(
-    k = map_dbl(modelo, "k"), 
+    k = map_dbl(modelo, "k"),
     w = map_dbl(modelo, "w")
   ) %>%
-    #map_dfr(~.x) %>%
-    summarise(across(c(k, w), moda))
-  
-  #list_param <- list()
-  #list_param[[1]] <- c(k_mean, w_mean)
+  #map_dfr(~.x) %>%
+  summarise(across(c(k, w), moda))
 }
+
+get_meanpar <- function(modelo){
+  tibble(
+    k = map_dbl(modelo,"k"),
+    w = map_dbl(modelo,"w")
+  ) %>% 
+    summarize(
+      mean = across(c(k,w), mean_wise),
+      mean = round(mean,0)
+    )
+}
+
 
 
 get_mpred <- function(lista) {
